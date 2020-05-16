aa546b53af9d7a453934dfd277c7e1608ae90172
Ramon Audain <raudain123@gmail.com>
2020-05-16 21:40:27 +0000

    Solution #3 -- Not working

--- main.js
+++ main.js
@@ -2,26 +2,53 @@
 // The fibonacci series is an ordering of numbers where each number is the sume of the preceeding two.
 
 
-function fibonacci(n){
-  // Solution #1
-  const result = [0, 1];
+// function fibonacci(n){
+//   // Solution #1
+//   const result = [0, 1];
 
-  for (let i = 2; i <= n; i++){
-    const a = result[i - 1];
-    const b = result[i - 2];
+//   for (let i = 2; i <= n; i++){
+//     const a = result[i - 1];
+//     const b = result[i - 2];
 
-    result.push(a + b);
+//     result.push(a + b);
+//   }
+
+//   return result[n];
+
+
+//   // Solution #2
+//   if (n < 2){
+//     return n;
+//   }
+
+//   return fibonacci(n - 1) + fibonacci(n - 2);
+// }
+
+
+// Solution #3
+function memoize(fn){
+  const cache = {};
+
+  return function(...args){
+    if (cache[args]){
+      return cache[args];
+    }
+
+    const result = fn.apply(this, args);
+    cache[args] = result;
+
+    return result;
   }
+}
 
-  return result[n];
-
-
-  // Solution #2
-  if (n < 2){
+function fib(n){
+  if(n < 2){
     return n;
   }
 
-  return fibonacci(n - 1) + fibonacci (n - 2);
+  return fib(n - 1) + fib(n - 2);
 }
 
-fibonacci(10);
+// fibonacci(5);
+fib(15);
+memoize(fib);
