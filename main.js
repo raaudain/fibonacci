4a87a10abc59a2679481e692813e3fc93d75555f
Ramon Audain <raudain123@gmail.com>
2020-05-16 20:44:26 +0000

    Solution #2

--- main.js
+++ main.js
@@ -14,6 +14,14 @@ function fibonacci(n){
   }
 
   return result[n];
+
+
+  // Solution #2
+  if (n < 2){
+    return n;
+  }
+
+  return fibonacci(n - 1) + fibonacci (n - 2);
 }
 
-fibonacci(4);
+fibonacci(10);
