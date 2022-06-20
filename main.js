88d7c96be0d5de4e816303cf03cb1159c0c5ba8f
Ramon Audain <raudain123@gmail.com>
2022-06-20 04:26:10 +0000

    test

--- main.js
+++ main.js
@@ -2,43 +2,52 @@
 // The fibonacci series is an ordering of numbers where each number is the sume of the preceeding two.
 
 
-// function fibonacci(n){
-//   // Solution #1
-//   const result = [0, 1];
+function fibonacci(n){
+  // Solution #1
+  const result = [0, 1];
+  let c = 0
+  for (let i = 2; i <= n; i++){
+    const a = result[i - 1];
+    const b = result[i - 2];
 
-//   for (let i = 2; i <= n; i++){
-//     const a = result[i - 1];
-//     const b = result[i - 2];
-
-//     result.push(a + b);
-//   }
-
-//   return result[n];
+    result.push(a + b);
+    console.log(c, a + b)
+    c+=1
+  }
+  return result[n];
 
 
-//   // Solution #2
-//   if (n < 2){
-//     return n;
-//   }
+  // // Solution #2
+  // if (n < 2){
+  //   return n;
+  // }
 
-//   return fibonacci(n - 1) + fibonacci(n - 2);
-// }
+  // return fibonacci(n - 1) + fibonacci(n - 2);
+}
 
 
 // Solution #3
 
+
 function fib(n, memo={}){
+  const result = [0, 1]
   if (memo[n]){
-    console.log(memo[n]);
-    return memo[n];
+    result.push(memo[n]);
+    return memo[n]
   }
 
-  if(n < 2){
-    return n;
-  }
+  if(n === 0) return 0;
+  if(n === 1) return 1;
 
+  // if(n < 3){
+  //   return 1;
+  // }
+
+  console.log(fib(n - 1, memo) + fib(n - 2, memo))
   return memo[n] = fib(n - 1, memo) + fib(n - 2, memo);
+
 }
 
-// fibonacci(5);
-fib(50);
+fibonacci(20);
+// fib(50);
+//printFib(12)
